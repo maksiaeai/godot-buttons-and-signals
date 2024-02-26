92c4a54171134d7d2296ec79c3bff4d86ef34202
Jason Bennett <jasoben@users.noreply.github.com>
2024-02-26 13:55:07 -0500

    ready for takeoff

--- Scripts/Scripts Only for Homework Exercises/rotate_for_HW_scene_two.gd
+++ Scripts/Scripts Only for Homework Exercises/rotate_for_HW_scene_two.gd
@@ -1,5 +1,6 @@
 extends Node2D
 @export var rotation_speed : int 
+var is_rotating : bool
 
 
 # Called when the node enters the scene tree for the first time.
@@ -7,11 +8,15 @@ func _ready():
 	pass # Replace with function body.
 
 
-func _rotate_me():
-	transform = transform.rotated_local(rotation_speed)
+func _start_rotating():
+	is_rotating = true
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
+	if (is_rotating):
+		transform = transform.rotated_local(rotation_speed)
 	pass
+
+
 
 
